4ed853e4bd3713d723077cf4ad2d42a319bd0d9a
Aryansinha2811 <sinhaaryan622@gmail.com>
2023-11-23 22:27:01 +0530

    Solving some String Questions

---
+++ stringQuestions.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class stringQuestions {
+    public static void main(String[] args) {
+        
+        // Problem 1 :- Convert a UpperCase Sentence To LowerCase 
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a Sentence in a UpperCase Below :-");
+        String st = sc.nextLine();
+        System.out.println("Entered Sentence by You is :-");
+        System.out.println(st);
+        System.out.println("In Lower Case :--");
+        String lc = st.toLowerCase();
+        System.out.println(lc);
+
+        // Problem 2 :- Replace a Space( ) by Underscore (_)
+        System.out.println("Replacing Blank Space by a UnderScore (_) ");
+        st = st.replace(" " , "_");
+        System.out.println(st);
+    }
+}
